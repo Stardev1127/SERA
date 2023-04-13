cb37348be39aad46213d7f1959f9a1b4967f8cc4
Stardev1127 <cleverdragon0228@gmail.com>
2023-04-13 16:00:49 -0400

    Implemented business ecosystem page

--- sera_frontend_pro/src/content/Applications/BusinessEcosystem/PageHeader.tsx
+++ sera_frontend_pro/src/content/Applications/BusinessEcosystem/PageHeader.tsx
@@ -1,32 +1,41 @@
+import React, { useContext } from 'react';
 import { Typography, Button, Grid } from '@mui/material';
 
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
+import AddPartnerDialog from './AddPartnerDialog';
+import { SeraContext } from '@/contexts/SeraContext';
 
 function PageHeader() {
   const user = {
     name: 'Rory Porter',
     avatar: '/static/images/avatars/avatar.jpg'
   };
+  const { handleOpenAPDialog } = useContext(SeraContext);
+
   return (
-    <Grid container justifyContent="space-between" alignItems="center">
-      <Grid item>
-        <Typography variant="h3" component="h3" gutterBottom>
-          Business Ecosystem
-        </Typography>
-        <Typography variant="subtitle2">
-          {user.name}, these are your recent transactions
-        </Typography>
+    <>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="h3" component="h3" gutterBottom>
+            Business Ecosystem
+          </Typography>
+          <Typography variant="subtitle2">
+            {user.name}, these are your recent transactions
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            sx={{ mt: { xs: 2, md: 0 } }}
+            variant="contained"
+            startIcon={<AddTwoToneIcon fontSize="small" />}
+            onClick={() => handleOpenAPDialog()}
+          >
+            Add Business Partner
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Button
-          sx={{ mt: { xs: 2, md: 0 } }}
-          variant="contained"
-          startIcon={<AddTwoToneIcon fontSize="small" />}
-        >
-          Add Business Partner
-        </Button>
-      </Grid>
-    </Grid>
+      <AddPartnerDialog />
+    </>
   );
 }
 
